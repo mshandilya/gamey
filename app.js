15c0877e07e3d2bbdb2b6b9161f7a53a4ce6f33f
Mrigankashekhar Shandilya <65168739+mshandilya@users.noreply.github.com>
2024-11-20 07:16:19 +0530

    substandard mdp engine added

--- app.js
+++ app.js
@@ -97,65 +97,90 @@ class Game {
     }
 
     bestMoveMdp() {
-        let rewards = [], n_rewards = [], f_rewards = [];
-        let gamma = 1, max_iter = 100, max_ind = 0;
+        let p1rewards = [], p1n_rewards = [], p2rewards = [], p2n_rewards = [], f_rewards = [];
+        let gamma = 0.8, max_iter = 1000, beta = 0.2, delta = 1, max_ind = 0;
         for (let i = 0; i < this.nnodes; i++) {
             f_rewards.push(1);
-            rewards.push([0, 0, 0]);
-            n_rewards.push([0, 0, 0]);
+            p1rewards.push([0, 0, 0]);
+            p1n_rewards.push([0, 0, 0]);
+            p2rewards.push([0, 0, 0]);
+            p2n_rewards.push([0, 0, 0]);
         }
         switch (this.gameBoard) {
             case "93Board":
-                for (let i = 0; i < 9; i++)
-                    rewards[i][0] = 100;
-                for (let i = 8; i < 17; i++)
-                    rewards[i][1] = 100;
-                for (let i = 16; i < 24; i++)
-                    rewards[i][2] = 100;
-                rewards[0][2] = 100;
-                for(let i = 0; i<25; i++)
-                    if(this.nodePlayers[i]===3-this.currentPlayer)
-                        rewards[i] = [0, 0, 0];
+                for (let i = 0; i < 9; i++) {
+                    p1rewards[i][0] = 10;
+                    p2rewards[i][0] = 10;
+                }
+                for (let i = 8; i < 17; i++) {
+                    p1rewards[i][1] = 10;
+                    p2rewards[i][1] = 10;
+                }
+                for (let i = 16; i < 24; i++) {
+                    p1rewards[i][2] = 10;
+                    p2rewards[i][2] = 10;
+                }
+                p1rewards[0][2] = 10;
+                p2rewards[0][2] = 10;
+                for(let i = 0; i<24; i++) {
+                    if (this.nodePlayers[i] === 3 - this.currentPlayer)
+                        p1rewards[i] = [0, 0, 0];
+                    else if (this.nodePlayers[i] === this.currentPlayer)
+                        p2rewards[i] = [0, 0, 0];
+                }
                 break;
             default:
                 break;
         }
         for(let iter = 0; iter<max_iter; iter++) {
             for (let i = 0; i < this.nnodes; i++)
-                n_rewards[i] = [0, 0, 0];
+                for(let j = 0; j < 3; j++) {
+                    p1n_rewards[i][j] = 0;
+                    p2n_rewards[i][j] = 0;
+                }
             for (let i = 0; i < this.nnodes; i++) {
                 for (let j = 0; j < this.adjList[i].length; j++) {
-                    if(this.nodePlayers[j]===0) {
-                        n_rewards[i][0] += gamma * rewards[j][0];
-                        n_rewards[i][1] += gamma * rewards[j][1];
-                        n_rewards[i][2] += gamma * rewards[j][2];
+                    if(this.nodePlayers[this.adjList[i][j]]===0) {
+                        p1n_rewards[i][0] = Math.max(p1n_rewards[i][0], beta * p2rewards[i][0] + gamma * p1rewards[this.adjList[i][j]][0]);
+                        p1n_rewards[i][1] = Math.max(p1n_rewards[i][1], beta * p2rewards[i][1] + gamma * p1rewards[this.adjList[i][j]][1]);
+                        p1n_rewards[i][2] = Math.max(p1n_rewards[i][2], beta * p2rewards[i][2] + gamma * p1rewards[this.adjList[i][j]][2]);
+                        p2n_rewards[i][0] = Math.max(p2n_rewards[i][0], beta * p1rewards[i][0] + gamma * p2rewards[this.adjList[i][j]][0]);
+                        p2n_rewards[i][1] = Math.max(p2n_rewards[i][1], beta * p1rewards[i][1] + gamma * p2rewards[this.adjList[i][j]][1]);
+                        p2n_rewards[i][2] = Math.max(p2n_rewards[i][2], beta * p1rewards[i][2] + gamma * p2rewards[this.adjList[i][j]][2]);
                     }
-                    else if(this.nodePlayers[j]===this.currentPlayer) {
-                        n_rewards[i][0] += rewards[j][0];
-                        n_rewards[i][1] += rewards[j][1];
-                        n_rewards[i][2] += rewards[j][2];
+                    else if(this.nodePlayers[this.adjList[i][j]]===this.currentPlayer) {
+                        p1n_rewards[i][0] = Math.max(p1n_rewards[i][0], beta * p2rewards[i][0] + delta * p1rewards[this.adjList[i][j]][0]);
+                        p1n_rewards[i][1] = Math.max(p1n_rewards[i][1], beta * p2rewards[i][1] + delta * p1rewards[this.adjList[i][j]][1]);
+                        p1n_rewards[i][2] = Math.max(p1n_rewards[i][2], beta * p2rewards[i][2] + delta * p1rewards[this.adjList[i][j]][2]);
+                    }
+                    else {
+                        p2n_rewards[i][0] = Math.max(p2n_rewards[i][0], beta * p1rewards[i][0] + delta * p2rewards[this.adjList[i][j]][0]);
+                        p2n_rewards[i][1] = Math.max(p2n_rewards[i][1], beta * p1rewards[i][1] + delta * p2rewards[this.adjList[i][j]][1]);
+                        p2n_rewards[i][2] = Math.max(p2n_rewards[i][2], beta * p1rewards[i][2] + delta * p2rewards[this.adjList[i][j]][2]);
                     }
                 }
-                n_rewards[i][0] = Math.max(rewards[i][0], n_rewards[i][0]/this.adjList[i].length);
-                n_rewards[i][1] = Math.max(rewards[i][1], n_rewards[i][1]/this.adjList[i].length);
-                n_rewards[i][2] = Math.max(rewards[i][2], n_rewards[i][2]/this.adjList[i].length);
             }
             for (let i = 0; i < this.nnodes; i++)
                 if (this.nodePlayers[i] === 3-this.currentPlayer)
-                    n_rewards[i] = [0, 0, 0];
+                    p1n_rewards[i] = [0, 0, 0];
             for (let i = 0; i < this.nnodes; i++)
                 for(let j = 0; j<3; j++)
-                    rewards[i][j] = n_rewards[i][j];
+                    p1rewards[i][j] = p1n_rewards[i][j];
+            for (let i = 0; i < this.nnodes; i++)
+                if (this.nodePlayers[i] === this.currentPlayer)
+                    p2n_rewards[i] = [0, 0, 0];
+            for (let i = 0; i < this.nnodes; i++)
+                for(let j = 0; j<3; j++)
+                    p2rewards[i][j] = p2n_rewards[i][j];
         }
         for (let i = 0; i < this.nnodes; i++)
             for(let j = 0; j<3; j++)
-                f_rewards[i] *= rewards[i][j];
+                f_rewards[i] *= p1rewards[i][j];
         let available = [];
         for (let i = 0; i < this.nnodes; i++)
             if(this.nodePlayers[i] === 0)
                 available.push(i);
         available.sort((a, b) => f_rewards[b] - f_rewards[a]);
-        console.log("Returning " + available[0].toString());
         return available[0];
     }
 }
